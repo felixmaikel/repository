0958a7d865e69767b53eb00652bb1643e61d9e00
felixmaikel <felix.maikel@gmail.com>
2017-02-13 15:09:41 +0100

    Realizacion del ejemplo 5

--- angular/ejemplo5/src/app/app.component.ts
+++ angular/ejemplo5/src/app/app.component.ts
@@ -3,8 +3,45 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+      .red { color : red;}
+      .blue {color : blue;}
+      .yellow { color : yellow;}
+      .blod { color : blod}
+      `
+  ]
 })
+
 export class AppComponent {
-  title = 'app works!';
+  name = "Anybody";
+  className = "red";
+  redActive = true;
+  num = 2;
+  visible = false;
+
+  elems = [
+    { desc : "Elemet1", check:true},
+    { desc : "Elemet2", check:true},
+    { desc : "Elemet3", check:false}
+  ];
+
+  pClasses = {
+    red : true,
+    bold : false
+  };
+
+  setClassName(className : string){
+    this.className = className;
+    this.redActive = (className === 'red');
+  }
+
+  changeParagraph(){
+    this.pClasses.bold = true;
+  }
+
+  showText(){
+    this.visible = true;
+    this.num = 3;
+  }
+
 }
