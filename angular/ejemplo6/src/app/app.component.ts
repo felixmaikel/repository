8360605c2a560511364f4e418ed70b4360b91ec5
felixmaikel <felix.maikel@gmail.com>
2017-02-14 13:50:05 +0100

    Ejercicio 1
    Realización del ejercicio 1

--- angular/ejemplo6/src/app/app.component.ts
+++ angular/ejemplo6/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Item{
+  description : string;
+  status : boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  size = 0;
+  
+  elements : Item [] = [];
+
+  addItem(description : string){
+      this.elements.push({description,status:false});
+  }
+
+  removeItem(item : Item){
+    let index = this.elements.indexOf(item);
+    this.elements.splice(index,1);
+  }
 }
